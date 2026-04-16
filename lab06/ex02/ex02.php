2c1d8e0135e4f5bbccc601463f55d7f37b2a0d16
jan16hcm <tranhoangkhai271625@gmail.com>
2026-04-16 10:52:20 +0700

    Add ex01 and ex02

--- lab06/ex02/ex02.php
+++ lab06/ex02/ex02.php
@@ -1,3 +1,37 @@
+<?php 
+    $errorMess = "";
+    $res = "";
+    
+    if(!empty($_GET)) {
+        $n1 = isset($_GET["num1"]) ? trim($_GET["num1"]) : "";
+        $n2 = isset($_GET["num2"]) ? trim($_GET["num2"]) : "";
+        $op = isset($_GET["op"]) ? ($_GET["op"]) : "";
+        if($n1 === "" || $n2 === "") {
+            $errorMess = "Please enter full information";
+        } else {
+            $num1 = floatval($n1);
+            $num2 = floatval($n2);
+            switch ($op) {
+                case '+':
+                    $res = $num1 + $num2;
+                    break;
+                case '-':
+                    $res = $num1 - $num2;
+                    break;
+                case '*':
+                    $res = $num1 * $num2;
+                    break;
+                case '/':
+                    if($num2 == 0) {
+                        $errorMess = 'Can not divided by zero';
+                        break;
+                    }
+                    $res = $num1 / $num2;
+                    break;
+            }
+        }
+    }
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -21,79 +55,48 @@
                 <form>
                     <div class="form-group">
                         <label for="num1">Số hạng 1</label>
-                        <input type="text" class="form-control" id="num1" name="num1">
+                        <input type="text" class="form-control" id="num1" name="num1" value="<?php echo isset($_GET["num1"]) ? $_GET["num1"] : '' ; ?>">
                     </div>
                     <div class="form-group">
                         <label for="num2">Số hạng 2</label>
-                        <input type="text" class="form-control" id="num2" name="num2">
+                        <input type="text" class="form-control" id="num2" name="num2" value="<?php echo isset($_GET["num2"]) ? $_GET["num2"] : '' ; ?>">
                     </div>
                     <div class="form-group">
                         <div class="custom-control custom-radio custom-control-inline">
-                            <input checked value="+" name="op" id="add" type="radio" class="custom-control-input">
+                            <input id="add" type="radio" class="custom-control-input" name="op" value='+' checked>
                             <label for="add" class="custom-control-label">Cộng</label>
                         </div>
                         <div class="custom-control custom-radio custom-control-inline">
-                            <input value="-" name="op" id="subtract" type="radio" class="custom-control-input">
+                            <input id="subtract" type="radio" class="custom-control-input" name="op" value='-'>
                             <label for="subtract" class="custom-control-label">Trừ</label>
                         </div>
                         <div class="custom-control custom-radio custom-control-inline">
-                            <input value="*" name="op" id="multiply" type="radio" class="custom-control-input">
+                            <input id="multiply" type="radio" class="custom-control-input" name="op" value='*'>
                             <label for="multiply" class="custom-control-label">Nhân</label>
                         </div>
                         <div class="custom-control custom-radio custom-control-inline">
-                            <input value="/" name="op" id="divide" type="radio" class="custom-control-input">
+                            <input id="divide" type="radio" class="custom-control-input" name="op" value='/'>
                             <label for="divide" class="custom-control-label">Chia</label>
                         </div>
+                        <?php if($errorMess !== ""): ?>
+                            <div class="alert alert-danger" style="margin-top:5px">
+                                <?php echo $errorMess; ?>
+                            </div>
+                        <?php endif; ?>
                     </div>
                     <button class="btn btn-success">Xem kết quả</button>
                 </form>
             </div>
         </div>
-        <?php
-            if(empty($_GET)) {
-                return 0;
-            }
-            $num1 = intval($_GET["num1"]);
-            $num2 = intval($_GET["num2"]);
-            $op = $_GET["op"];
-            $res = $num1;
-            $errorMess = "";
-            if(empty($num1) || empty($num2) || empty($op)) {
-                $errorMess = "Please enter number";
-            } else {
-                switch ($op) {
-                    case "+":
-                        $res = $num1 + $num2;
-                        break;
-                    case "-":
-                        $res = $num1 - $num2;
-                        break;
-                    case "*":
-                        $res = $num1 * $num2;
-                        break;
-                    case "/":
-                        if($num2 == 0) {
-                            $errorMe = "Cannot divide by zero";
-                        } else {
-                            $res = $num1 / $num2;
-                        }
-                        break;
-                    }
-                }
-            
-        ?>
-        <div class="row">
-            <div class="col-md-6 mx-auto px-3 py-3 text-center">
-                <?php 
-                    if(empty($errorMess)) {
-                        echo "<div class='alert alert-success'>$num1 $op $num2 = $res</div>";
-                    } else {
-                        echo "<div class='alert alert-danger'>$errorMess</div>";
-                    }
-                ?>
+        <?php if ($res !== "" && $errorMess === ""): ?>
+            <div class="row">
+                <div class="col-md-6 mx-auto px-3 py-3 text-center">
+                    <div class="alert alert-success">
+                        <?php echo "$num1 $op $num2 = $res"; ?>
+                    </div>
+                </div>
             </div>
-        </div>
+        <?php endif; ?>
     </div>
-    
 </body>
 </html>
