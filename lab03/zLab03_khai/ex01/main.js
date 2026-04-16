3b0afe007a964ef9d16ddaf13dedf61299f0a289
Tran Hoang Khai <tranhoangkhai271625@gmail.com>
2026-04-16 13:18:13 +0700

    .

--- lab03/zLab03_khai/ex01/main.js
+++ lab03/zLab03_khai/ex01/main.js
@@ -1,89 +1,44 @@
-// const email = document.getElementById("email");
-// const password = document.getElementById("pwd");
-// const error = document.querySelector(".errorMessage");
-// const form = document.querySelector("form");
+const email = document.getElementById("email");
+const password = document.getElementById("pwd");
+const error = document.querySelector("#errorMessage");
+const btnSubmit = document.getElementById("btn-submit");
+const form = document.querySelector("form");
 
-// function showError(message , input = null) {
-//     error.innerHTML = message;
-//     error.classList.remove("d-none");
-
-//     if(input) {
-//         input.classList.add("is-invalid");
-//         input.focus();
-//     }
-// }
-
-// email.addEventListener("invalid", (e) => {
-//     e.preventDefault();
-//     showError("Your email is not correct!", email);
-// });
-
-// email.addEventListener("input", () => {
-//     error.classList.add("d-none");
-//     email.classList.remove("is-invalid");
-// });
-
-
-// password.addEventListener("input", () => {
-//     error.classList.add("d-none");
-//     password.classList.remove("is-invalid");
-// });
-
-
-// form.addEventListener("submit", (e) => {
-//     if(email.value.trim() === "") {
-//         e.preventDefault();
-//         showError("Please enter your email", email);
-//     } 
-//     else if(password.value.trim() === "") {
-//         e.preventDefault();
-//         showError("Please enter your password!", password);
-//     }
-//     else if(password.value.trim().length < 6) {
-//         e.preventDefault();
-//         showError("Your password must contain at least 6 characters", password);
-//     }
-// });
-
-const email = document.querySelector("#email")
-const password = document.querySelector("#pwd")
-const error = document.querySelector(".errorMessage")
-const form = document.querySelector("form")
-
-function showMessage(message, input = null) {
-    error.innerHTML = message
-    error.classList.remove("d-none")
+function updateMess(message, input = null) {
+    error.classList.remove("d-none");
+    error.innerHTML = message;
 
     if(input) {
-        input.classList.add("is-invalid")
-        input.focus()
+        input.focus();
+        input.classList.add("is-invalid");
     }
 }
 email.addEventListener("invalid", (e) => {
-    e.preventDefault()
-    showMessage("Your email is not correct", email)
+    e.preventDefault();
+    updateMess("Your email is not correct", email);
 })
-email.addEventListener("input", () => {
-    error.classList.add("d-none")
-    email.classList.remove("is-invalid")
+email.addEventListener(("input"), () => {
+    email.classList.remove("is-invalid");
+    error.classList.add("d-none");
 })
 
-password.addEventListener("input", () => {
-    error.classList.add("d-none")
-    password.classList.remove("is-invalid")
+password.addEventListener(("input"), () => {
+    password.classList.remove("is-invalid");
+    error.classList.add("d-none");
 })
+
 
 form.addEventListener("submit", (e) => {
     if(email.value.trim() === "") {
-        e.preventDefault()
-        showMessage("Please enter your email", email)
+        e.preventDefault();
+        updateMess("Please enter your email", email);
+    } else if(password.value.trim() === "") {
+        e.preventDefault();
+        updateMess("Please enter your password", password);
+    } else if(password.value.trim().length < 6) {
+        e.preventDefault();
+        updateMess("Your password must contain at least 6 characters", password);
     }
-    else if(password.value.trim() === "") {
-        e.preventDefault()
-        showMessage("Please enter your password", password)
-    }
-    else if(password.value.trim().length < 8) {
-        e.preventDefault()
-        showMessage("Your password must contain at least 6 characters", password)
-    }
-})
+});
+
+
